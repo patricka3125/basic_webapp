f9c003d1bfda92d3ff235abcf05bb20d6f984888
Patrick Liao <patrick@MacBook-Pro.appisocial.com>
2017-09-08 18:34:27 +0800

    Debugged messages and groups

--- models/groups-sequelize.js
+++ models/groups-sequelize.js
@@ -20,14 +20,13 @@ exports.connectDB = function() {
       else resolve(data);
     });
   })
-  .then(yamltext => {
-    jsyaml.safeLoad(yamltext, 'utf8');
-  })
+  .then(yamltext => jsyaml.safeLoad(yamltext, 'utf8'))
   .then(params => {
     if(!sequlz) sequlz = new Sequelize(params.dbname, params.username, params.password, params.params);
 
-    if(!SQGroup) SQGroup = sequlz.define('Group_Users' {
-      group_id: { type: Sequelize.INTEGER, autoIncrement: true } // unique identifier for a group
+    if(!SQGroup) SQGroup = sequlz.define('Group_Users', {
+      uuid: { type: Sequelize.UUID, primaryKey: true },
+      group_id: { type: Sequelize.INTEGER, autoIncrement: true}, // unique identifier for a group
       group_name: Sequelize.STRING, // Name of the group that will be displayed
       username: Sequelize.STRING  // User that belongs in the group
     });
@@ -35,23 +34,27 @@ exports.connectDB = function() {
   });
 };
 
-exports.createGroup() = function(group_name, username) {
-  exports.connectDB()
-  .then(SQGroup => SQGroup.create({ group_name, username }));
+exports.createGroup = function(group_name, username) {
+  return exports.connectDB()
+  .then(SQGroup => {
+    return SQGroup.create({ 
+      group_name, username 
+    });
+  });
 };
 
-exports.addToGroup() = function(group_id, username) {
-  exports.connectDB()
+exports.addToGroup = function(group_id, username) {
+  return exports.connectDB()
   .then(SQGroup => SQGroup.find({ where: { group_id: group_id }, rejectOnEmpty: true }))
-  .then(group => SQGroup.create({ group_id, group.group_name, username }))
+  .then(group => SQGroup.create({ group_id, group_name: group.group_name, username }))
   .catch(err => {
     error(err.message);
     error(err.stack);
   });
 };
 
-exports.deleteFromGroup() = function(group_id, username) {
-  exports.connectDB()
+exports.deleteFromGroup = function(group_id, username) {
+  return exports.connectDB()
   .then(SQGroup => SQGroup.find({ where: { group_id: group_id, username: username } }))
   .then(user => user.destroy())
   .catch(err => {
--- models/messages-sequelize.js
+++ models/messages-sequelize.js
@@ -20,13 +20,11 @@ exports.connectDB = function(db_name) {
       else resolve(data);
     });
   })
-  .then(yamltext => {
-    jsyaml.safeLoad(yamltext, 'utf8');
-  })
+  .then(yamltext => jsyaml.safeLoad(yamltext, 'utf8'))
   .then(params => {
     if(!sequlz) sequlz = new Sequelize(params.dbname, params.username, params.password, params.params);
 
-    if(!SQMessage) SQMessage = sequlz.define('Message' {
+    if(!SQMessage) SQMessage = sequlz.define('Message', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
       from: Sequelize.STRING, // user that sent the message
       group_id: Sequelize.INTEGER, // unique id of the chat group
@@ -37,24 +35,25 @@ exports.connectDB = function(db_name) {
   });
 };
 
-exports.sendMessage = function(from, group_name, message) {
+exports.sendMessage = function(from, group_id, message) {
   exports.connectDB('Message')
   .then(SQMessage => SQMessage.create({
-    from, group_name, message, timestamp: new Date())
-  })
+    from, group_id, message, timestamp: new Date()
+  }))
   .then(newmsg => {
     var toEmit = {
       id: newmsg.id,
       from: newmsg.from,
-      group_name: newmsg.group_name,
+      group_id: newmsg.group_id,
       message: newmsg.message,
       timestamp: newmsg.timestamp
-    });
+    };
   });
-}
+};
 
 exports.deleteMessage = function(id) {
   exports.connectDB('Message')
   .then(SQMessage => SQMessage.find({ where: { id } }))
   .then(msg => msg.destroy());
 };
+
